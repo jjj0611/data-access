46eec317f43098c29e13cf717435261884b0e1a1
Jaeju Jang <jaeju.island@woowahan.com>
2021-03-26 14:16:25 +0900

    jdbc api를 사용하는 모듈 추가

---
+++ data-access-implementation/data-jdbc-api/src/main/java/org/kiworkshop/jdbcapi/MemberDaoJdbcApiImpl.java
@@ -0,0 +1,15 @@
+package org.kiworkshop.jdbcapi;
+
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class MemberDaoJdbcApiImpl {
+
+    private DataSource dataSource;
+
+//    public MemberDaoJdbcApiImpl(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+}
